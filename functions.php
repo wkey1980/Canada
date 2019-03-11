77e57d7f923a5a878e409f5ac274c03e11181980
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2019-03-11 11:19:30 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,36 @@
 <?php 
 
-function canada_theme_setup() {
+function canada_setup() {
+    load_textdomain( 'canada', get_template_directory() . '/languages' );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' 150, 150, array(
+        'center', 'center'
+    ));
+    register_nav_menus( array(
+        'menu-1' => __( 'Primary Menu', 'canada' ),
+        'menu-2' => __( 'Footer Menu', 'canada' ),
+    ));
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ));
+    add_theme_support( 'custom logo', array(
+        'height' => 190,
+        'width' => 190,
+        'flex-height' => true,
+        'flex-width' => true,
+        'header-text' => array(
+            'site-title',
+            'site-description'
+        ),
 
+    ));
+    
 }
-add_action( 'after_theme_setup', 'canada_theme_setup' );
+add_action( 'after_theme_setup', 'canada_setup' );
 
 require get_template_directory() . '/inc/customizer.php';
