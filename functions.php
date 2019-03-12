4f944313bfb8f93a9bee4b97a9fff38bbcbfeff2
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2019-03-12 09:57:42 +0000

    Updated Theme Code

--- functions.php
+++ functions.php
@@ -4,9 +4,9 @@ function canada_setup() {
     load_textdomain( 'canada', get_template_directory() . '/languages' );
     add_theme_support( 'automatic-feed-links' );
     add_theme_support( 'title-tag' );
-    add_theme_support( 'post-thumbnails' 150, 150, array(
-        'center', 'center'
-    ));
+    
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 150, 150 );
     register_nav_menus( array(
         'menu-1' => __( 'Primary Menu', 'canada' ),
         'menu-2' => __( 'Footer Menu', 'canada' ),
@@ -29,8 +29,47 @@ function canada_setup() {
         ),
 
     ));
-    
+    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support( 'wp-block-styles' );
+    add_theme_support( 'align-wide' );
+    add_theme_support( 'editor-styles' );
+    add_theme_support( 'style-editor.css' );
+    add_theme_support( 'responsive-embeds' );
 }
 add_action( 'after_theme_setup', 'canada_setup' );
 
+function canada_scripts() {
+    wp_enqueue_style( 'default_style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'canada_scripts' );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 require get_template_directory() . '/inc/customizer.php';
+
+
+
+
+
+
+
+
+
+
+
